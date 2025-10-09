5c66257a9b6a620b4a249453509057fcb52cef44
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-10-09 13:14:21 +0100

    Change on-screen text

--- sandwich_shop/lib/main.dart
+++ sandwich_shop/lib/main.dart
@@ -54,7 +54,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -104,10 +103,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Welcome to my shop!',
+              style: const TextStyle(fontSize: 36),
             ),
           ],
         ),
