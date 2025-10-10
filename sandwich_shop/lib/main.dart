774de1a52ec72a228e2e7a519180f6a3ce65ec6d
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-10-10 14:16:36 +0100

    Increase text size

--- sandwich_shop/lib/main.dart
+++ sandwich_shop/lib/main.dart
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title, style: const TextStyle(fontSize: 32)),
       ),
       body: const Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               'Welcome to my shop!',
-              style: TextStyle(fontSize: 36),
+              style: TextStyle(fontSize: 42),
             ),
           ],
         ),
